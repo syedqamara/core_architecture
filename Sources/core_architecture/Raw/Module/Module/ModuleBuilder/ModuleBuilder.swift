ce210d66a74faa1bd1ed8cc3d3e2f61f07fa5a63
Qamar Abbas <syedqamar.a1@gmail.com>
2023-11-22 23:33:57 +0500

    Update ModuleBuilder.swift

--- Sources/core_architecture/Raw/Module/Module/ModuleBuilder/ModuleBuilder.swift
+++ Sources/core_architecture/Raw/Module/Module/ModuleBuilder/ModuleBuilder.swift
@@ -6,30 +6,72 @@
 //
 
 import Foundation
+import SwiftUI
 
-/// Protocol for a factory that creates views.
+/// A protocol for factories that create SwiftUI views based on input data.
+public protocol SwiftUIViewFactoryProtocol {
+    /// Creates a view of a specific type for a given input.
+    ///
+    /// - Parameter input: The input data for configuring the view.
+    /// - Returns: An instance of a view that conforms to the `SwiftUIView` protocol.
+    func makeView<I>(input: I) -> some SwiftUIView
+}
+
+/// A concrete implementation of `SwiftUIViewFactoryProtocol`.
+public struct SwiftUIViewFactory: SwiftUIViewFactoryProtocol {
+    /// Creates an instance of `SwiftUIViewFactory`.
+    public init() {}
+
+    /// Creates a `NoSwiftUIView` with a default view model.
+    public func makeView<I>(input: I) -> some SwiftUIView {
+        NoSwiftUIView(viewModel: .init())
+    }
+}
+
+/// A protocol for factories that create views based on input data.
 public protocol ViewingFactory {
     /// Creates a view of a specific type for a given input.
     ///
     /// - Parameter input: The input data for configuring the view.
-    /// - Returns: An instance of a view that conforms to the `Viewing` protocol.
-    func makeView<I>(input: I) -> any ViewProtocol
+    /// - Returns: An instance of a view that conforms to the `ViewProtocol` protocol.
+    func makeView<I>(input: I) -> some ViewProtocol
 }
 
+/// A concrete implementation of `ViewingFactory`.
 public struct ViewFactory: ViewingFactory {
+    /// Creates an instance of `ViewFactory`.
     public init() {}
-    
-    public func makeView<I>(input: I) -> any ViewProtocol {
+
+    /// Creates a `NoView` with a default view model.
+    public func makeView<I>(input: I) -> some ViewProtocol {
         NoView(viewModel: .init())
     }
-    
 }
 
-private  class NoViewModel: ViewModeling {}
+/// A private class representing a view model for a view.
+private class NoViewModel: ViewModeling {}
 
+/// A struct representing a view that conforms to the `ViewProtocol` protocol.
 private struct NoView: ViewProtocol {
     public typealias ViewModelType = NoViewModel
-    public init(viewModel: NoViewModel) {
-        
+    
+    /// Creates a `NoView` with the provided view model.
+    ///
+    /// - Parameter viewModel: The view model for the view.
+    public init(viewModel: NoViewModel) {}
+}
+
+/// A struct representing a SwiftUI view that conforms to the `SwiftUIView` protocol.
+private struct NoSwiftUIView: SwiftUIView {
+    public typealias ViewModelType = NoViewModel
+    
+    /// Creates a `NoSwiftUIView` with the provided view model.
+    ///
+    /// - Parameter viewModel: The view model for the SwiftUI view.
+    public init(viewModel: NoViewModel) {}
+    
+    /// The body of the SwiftUI view.
+    public var body: some View {
+        Text("")
     }
 }

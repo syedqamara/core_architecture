9604921cf58cedc7ba81f07ea72218281f6783c1
Qamar Abbas <syedqamar.a1@gmail.com>
2024-03-23 03:41:34 +0500

    Fixed Logging Issues and make it more controlable

--- Sources/Core/Logger/Logging.swift
+++ Sources/Core/Logger/Logging.swift
@@ -6,13 +6,48 @@
 //
 
 import Foundation
-
+import OSLog
 public protocol LogAction {
     var rawValue: String { get }
 }
-
+private let lineBarTextConstant = "\n\n--------------------------------------------\n\n"
 public class Logs<L: LogAction> {
+    public enum LogType {
+        case warning(configID: String), info(configID: String), error(configID: String)
+        
+        var configID: String {
+            switch self {
+            case .warning(let configID):
+                return configID
+            case .info(let configID):
+                return configID
+            case .error(let configID):
+                return configID
+            }
+        }
+        var type: String {
+            switch self {
+            case .warning(_):
+                return "warning"
+            case .info(_):
+                return "info"
+            case .error(_):
+                return "error"
+            }
+        }
+        var sign: String {
+            switch self {
+            case .warning(_):
+                return "⚠️ "
+            case .info(_):
+                return "‼️ "
+            case .error(_):
+                return "❌ "
+            }
+        }
+    }
     private let id: String
+    private var logs: [String: String] = [:]
     public init(id: String) {
         self.id = id
     }
@@ -23,25 +58,49 @@ public class Logs<L: LogAction> {
         Log Info:  \(completion() ?? "")
         """
     }
-    
-    public func log(_ data: Data?, action: L) {
-        print(printStructuredInfo(completion: data?.string ?? "", action: action))
+    // Log must be perform in order to push all the requested log flow for specific configID.
+    public func log(log: LogType) {
+        guard let value = logs[log.configID] else { return }
+        let logger = Logger(subsystem: log.type, category: log.configID)
+        switch log {
+        case .warning(_):
+            print("\(value)")
+        case .info(_):
+            print("\(value)")
+        case .error(_):
+            print("\(value)")
+        }
+    }
+    private func storeLogs(content: String, type: LogType) {
+        let beautifyContent = type.sign + content + type.sign
+        if let value = logs[type.configID] {
+            let finalLog = value + lineBarTextConstant + content
+            logs[type.configID] = finalLog
+        } else {
+            logs[type.configID] = content
+        }
+    }
+}
+
+extension Logs {
+    public func trackLog(type: LogType,_ data: Data?, action: L) {
+        logs[type.configID] = printStructuredInfo(completion: data?.string ?? "", action: action)
     }
     
-    public func log(_ dictionary: [String: Any]?, action: L) {
-        print(printStructuredInfo(completion: dictionary?.description, action: action))
+    public func trackLog(type: LogType,_ dictionary: [String: Any]?, action: L) {
+        logs[type.configID] = printStructuredInfo(completion: (dictionary ?? [:]).description, action: action)
     }
     
-    public func log(_ any: Any?, action: L) {
-        print(printStructuredInfo(completion: String(describing: any), action: action))
+    public func trackLog(type: LogType,_ any: Any?, action: L) {
+        logs[type.configID] = printStructuredInfo(completion: String(describing: any), action: action)
     }
-    public func log(_ any: URLRequest?, action: L) {
-        print(printStructuredInfo(completion: String(describing: any), action: action))
+    public func trackLog(type: LogType,_ any: URLRequest?, action: L) {
+        logs[type.configID] = printStructuredInfo(completion: String(describing: any), action: action)
     }
-    public func log(_ any: URL?, action: L) {
-        print(printStructuredInfo(completion: String(describing: any), action: action))
+    public func trackLog(type: LogType,_ any: URL?, action: L) {
+        logs[type.configID] = printStructuredInfo(completion: String(describing: any), action: action)
     }
-    public func log(_ any: Error?, action: L) {
-        print(printStructuredInfo(completion: String(describing: any), action: action))
+    public func trackLog(type: LogType,_ any: Error?, action: L) {
+        logs[type.configID] = printStructuredInfo(completion: String(describing: any), action: action)
     }
 }
--- Tests/CoreTests/CoreTests.swift
+++ Tests/CoreTests/CoreTests.swift
@@ -2,7 +2,14 @@ import XCTest
 @testable import Core
 
 final class CoreTests: XCTestCase {
+    
     func testExample() throws {
+        enum LogingAction: String, LogAction {
+            case log
+        }
+        let logger = Logs<LogingAction>(id: "123")
+        logger.trackLog(type: .warning(configID: "1122"), ["example_JSON_key": "Json Object"], action: .log)
+        logger.log(log: .warning(configID: "1122"))
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
